6be4650d3c3491997fdb92d6c9c7a63a0d368667
Nayun Kim <narchitect@icloud.com>
2022-07-11 17:59:09 +0900

    Example_Palindrome
    use for conditional statement.

--- DSC_Ex_palindrome.py
+++ DSC_Ex_palindrome.py
@@ -1,20 +1,14 @@
 def is_palindrome(word):
+    central_word_num = int(len(word)//2)
     
-    words_len = len(word)
-    central_num= int((words_len-1)/2)
-    front_words = word[:central_num]
-    reversed_words = word[::-1]
-    back_words = reversed_words[:central_num]
- 
+    for i in range(central_word_num):
+        front_word = word[i]
+        back_word = word [-(i+1)]
+        if front_word != back_word:
+            return False
+        elif i == central_word_num-1:
+            return True
     
-    if (words_len%2) == 0:
-        return False
-    elif front_words == back_words:
-        return True
-    else:
-        return False
-    
-
 print(is_palindrome("racecar"))
 print(is_palindrome("stars"))
 print(is_palindrome("토마토"))
